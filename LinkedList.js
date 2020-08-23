e00c99d2a5b5c6887fcc297794009dc1e7f8aa52
Martin Graves 422 <33223356+markpackham@users.noreply.github.com>
2020-08-23 08:26:41 +0100

    Test we can output linked list

--- LinkedList.js
+++ LinkedList.js
@@ -1,1 +1,20 @@
-const LinkedList = require("./LinkedList");
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.length = 0;
+  }
+  insertAtHead(data) {
+    const newNode = new LinkedListNode(data, this.head);
+    this.head = newNode;
+    this.length++;
+  }
+}
+
+class LinkedListNode {
+  constructor(value, next) {
+    this.value = value;
+    this.next = next;
+  }
+}
+
+module.exports = LinkedList;
--- app.js
+++ app.js
@@ -1,7 +1,13 @@
-// LinkedList with TTD learned from https://www.youtube.com/watch?v=gJjPWA8wpQg
+// LinkedList with TDD learned from https://www.youtube.com/watch?v=gJjPWA8wpQg
 const LinkedList = require("./LinkedList");
 
-const ll = LinkedList.fromValues(10, 20, 30, 40);
-ll.print();
-ll.removeAtIndex(2);
-ll.print();
+// const ll = LinkedList.fromValues(10, 20, 30, 40);
+// ll.print();
+// ll.removeAtIndex(2);
+// ll.print();
+
+const ll = new LinkedList();
+ll.insertAtHead(10);
+ll.insertAtHead(20);
+
+console.log(11);
