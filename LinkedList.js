e2a305feb2b1f077ff5d2bb92469ab81b3784146
Martin Graves 422 <33223356+markpackham@users.noreply.github.com>
2020-08-23 09:03:28 +0100

    Test get index

--- LinkedList.js
+++ LinkedList.js
@@ -9,6 +9,18 @@ class LinkedList {
     this.head = newNode;
     this.length++;
   }
+
+  getByIndex(index) {
+    // Arrays are faster than LinkedLists when searching for something
+    // return null if search is out of bounds
+    if (index < 0 || index >= this.length) return null;
+
+    let current = this.head;
+    for (let i = 0; i < index; i++) {
+      current = current.next;
+    }
+    return current;
+  }
 }
 
 class LinkedListNode {
@@ -17,5 +29,13 @@ class LinkedListNode {
     this.next = next;
   }
 }
+
+LinkedList.fromValues = function (...values) {
+  const ll = new LinkedList();
+  for (let i = values.length - 1; i >= 0; i--) {
+    ll.insertAtHead(values[i]);
+  }
+  return ll;
+};
 
 module.exports = LinkedList;
